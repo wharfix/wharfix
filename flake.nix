b6e60bc344e06cccde929a83758f2bc420b43a33
Johan Thomsen <jth@dbc.dk>
2022-07-11 04:36:33 +0200

    crate2nix -> cargo2nix

--- flake.nix
+++ flake.nix
@@ -2,31 +2,47 @@
   description = "wharfix";
 
   inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix";
+    cargo2nix.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, cargo2nix, nixpkgs }:
   let
     pname = "wharfix";
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
-      overlays = [ self.overlay ];
+      overlays = [ self.overlay cargo2nix.overlays.default ];
     };
+    lib = nixpkgs.lib;
+    cratePackage = _features: 
+    let
+      features = lib.naturalSort _features;
+      name = if features == ["default"] then pname else "${pname}-${lib.concatStringsSep "-" features}";
+    in
+    {
+      "${name}" = (pkgs.rustBuilder.makePackageSet {
+        rustVersion = pkgs.rustc.version;
+        packageFun = import ./Cargo.nix;
+        rootFeatures = map (f: "${pname}/${f}") features;
+      }).workspace.${pname} {};
+    };
+    outputFeatureSets = [
+      ["default"]
+      ["mysql"]
+    ];
+    outputPackages = lib.foldr (a: b: a // b) {} (map cratePackage outputFeatureSets);
   in {
-    packages.${system}.${pname} = pkgs.${pname};
-    defaultPackage.${system} = pkgs.${pname};
+    packages.${system} = outputPackages;
+    defaultPackage.${system} = outputPackages.wharfix;
 
-    overlay = final: prev: {
-      "${pname}" = (import ./Cargo.nix {
-        pkgs = final;
-      }).rootCrate.build;
-    };
+    overlay = final: prev: outputPackages;
 
     devShell.${system} = with pkgs; mkShell {
       buildInputs = [
         cargo
-        crate2nix
+        cargo2nix.packages.${system}.cargo2nix
         nix
         openssl.dev
         pkgconfig
