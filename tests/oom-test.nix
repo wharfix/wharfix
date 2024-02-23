99eeb868eac0422263206ce73adbf34f18edef08
Johan Thomsen <write@ownrisk.dk>
2024-02-24 00:28:30 +0100

    tests: make oom-test faster and not depend on wharfix-examples

--- tests/oom-test.nix
+++ tests/oom-test.nix
@@ -3,30 +3,16 @@
   nixpkgs ? builtins.fetchTarball "https://github.com/nixOS/nixpkgs/archive/22.05.tar.gz",
   pkgs ? import nixpkgs {},
 } : rec {
-  examplesRepo = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
-    pname = "examples";
-    version = "0";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "wharfix";
-      repo = "examples";
-      rev = "c09ebe2355b69f919f0dd2197d3d4c609fb6fb33";
-      hash = "sha256-sKvmqbMvJeLv0vo7kgVBdqSuKsOFdUXsbizlrVwxjlA=";
-    };
-
-
-    dontConfigure = true;
-    dontBuild = true;
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/
-      cp $src $out/repo -r
-
-      runHook postInstall
-    '';
-  });
+  repo = pkgs.runCommandNoCC "repo" { nativeBuildInputs = [pkgs.git]; } ''
+    mkdir -p $out
+    pushd $out
+    git init
+    cp ${./res/new-index.nix} $out/default.nix
+    git add .
+    git config user.email "example@example.com";
+    git config user.name "test";
+    git commit -m "Initial commit"
+  '';
   type = "app";
   default = program;
   program = pkgs.nixosTest {
@@ -38,32 +24,25 @@
         virtualisation.diskSize = 100240;
         virtualisation.writableStoreUseTmpfs = false;
 
+        # this defaults to 2 in the test framework,
+        # but we just want the test to fail on oom,
+        # not the vms in reboot loops
+        boot.kernel.sysctl."vm.panic_on_oom" = 0;
+
         environment.systemPackages = with pkgs; [
           git
-          (pkgs.writeShellScriptBin "make-repo" ''
-            # ${pkgs.coreutils}/bin/cp ${self} /etc/repo -r;
-            ${pkgs.coreutils}/bin/cp ${examplesRepo}/repo /etc/repo -r;
-            cd /etc/repo;
-            ${pkgs.git}/bin/git init;
-            ${pkgs.git}/bin/git config user.email "example@example.com";
-            ${pkgs.git}/bin/git config user.name "test";
-            ${pkgs.git}/bin/git remote add origin file:///etc/repo;
-            ${pkgs.git}/bin/git add .
-            ${pkgs.git}/bin/git commit -m "test";
-          '')
-          (pkgs.writeShellScriptBin "run-wharfix" ''
-            cd /etc/repo/;
-            ${pkgs.wharfix}/bin/wharfix --repo file:///etc/repo --port 8080;
-          '')
         ];
         systemd.services.wharfix = {
           wantedBy = [ "multi-user.target" ];
           after = [ "network.target" ];
           path = [ pkgs.nix ];
+          environment.NIX_PATH = "nixpkgs=${pkgs.path}";
           serviceConfig = {
+            Slice = "wharfix.slice";
+            MemoryAccounting = true;
+            MemoryMax = "256M";
             Environment = "RUST_BACKTRACE=1";
-            ExecStartPre = ''/run/current-system/sw/bin/make-repo'';
-            ExecStart = ''/run/current-system/sw/bin/run-wharfix'';
+            ExecStart = ''${pkgs.wharfix}/bin/wharfix --repo ${repo} --port 8080'';
           };
         };
 
@@ -78,14 +57,13 @@
     };
 
     testScript = ''
-          client1.start()
-          client1.wait_for_unit("docker.service")
+          start_all()
 
-          registry.start()
+          client1.wait_for_unit("docker.service")
           registry.wait_for_open_port(8080)
           registry.wait_for_unit("wharfix.service")
 
-          client1.succeed("docker pull registry:8080/hugepkg:master")
+          client1.succeed("docker pull registry:8080/kubernetes:master")
         '';
   };
 }
--- tests/res/new-index.nix
+++ tests/res/new-index.nix
@@ -3,5 +3,9 @@
     name = "new-cow";
     config.EntryPoint = [ "${pkgs.cowsay}/bin/cowsay new" ];
   };
+  kubernetes = {
+    name = "kubernetes";
+    contents = [ pkgs.kubernetes ];
+  };
   inherit pkgs;
 }
