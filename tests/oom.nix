7ab290ae65b2b18665c77e9cd7103864d54d9750
Johan Thomsen <write@ownrisk.dk>
2024-02-26 14:22:17 +0100

    tests/oom: a little cooler now with explicit flake dependency on 'old-self' as well as two separate test cases for success and failure

--- tests/oom.nix
+++ tests/oom.nix
@@ -1,4 +1,5 @@
 {
+  expectedResult ? "succeed",
   git,
   nix,
   nixosTest,
@@ -16,75 +17,56 @@ let
     git config user.name "test";
     git commit -m "Initial commit"
   '';
-  registryNode = package: nixpkgsPath: { # TODO: do something cooler than nixpkgsPath
-    virtualisation.memorySize = 4096;
-    virtualisation.diskSize = 100240;
-    virtualisation.writableStoreUseTmpfs = false;
-
-    # this defaults to 2 in the test framework,
-    # but we just want the test to fail on oom,
-    # not the vms in reboot loops
-    boot.kernel.sysctl."vm.panic_on_oom" = 0;
-
-    environment.systemPackages = [
-      git
-    ];
-    systemd.services.wharfix = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      path = [ nix ];
-      environment.NIX_PATH = "nixpkgs=${nixpkgsPath}";
-      serviceConfig = {
-        Slice = "wharfix.slice";
-        MemoryAccounting = true;
-        MemoryMax = "256M";
-        Environment = "RUST_BACKTRACE=1";
-        ExecStart = ''${package}/bin/wharfix --repo ${repo} --port 8080'';
-      };
-    };
-
-    networking.firewall.allowedTCPPorts = [ 8080 ];
-  };
-  nonSteamingWharfix =
-    (builtins.getFlake
-    "github:wharfix/wharfix/1f71fcafbc9caed5fa5d38f01598aaadb6176e08")
-    .defaultPackage.x86_64-linux;
 in
 nixosTest {
     name = "oom-test";
 
     nodes = {
-      registry1 = { pkgs, ... }:
-        registryNode wharfix pkgs.path;
+      registry = { pkgs, ... }: {
+        virtualisation.memorySize = 4096;
+        virtualisation.diskSize = 100240;
+        virtualisation.writableStoreUseTmpfs = false;
 
-      registry2 = { pkgs, ... }:
-        registryNode nonSteamingWharfix pkgs.path;
+        # this defaults to 2 in the test framework,
+        # but we just want the test to fail on oom,
+        # not the vms in reboot loops
+        boot.kernel.sysctl."vm.panic_on_oom" = 0;
 
-      client1 = { ... }: {
-        virtualisation.diskSize = 40240;
-        virtualisation.docker.enable = true;
-        virtualisation.docker.extraOptions = "--insecure-registry registry1:8080";
+        environment.systemPackages = [
+          git
+        ];
+        systemd.services.wharfix = {
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network.target" ];
+          path = [ nix ];
+          environment.NIX_PATH = "nixpkgs=${pkgs.path}";
+          serviceConfig = {
+            Slice = "wharfix.slice";
+            MemoryAccounting = true;
+            MemoryMax = "256M";
+            Environment = "RUST_BACKTRACE=1";
+            ExecStart = ''${wharfix}/bin/wharfix --repo ${repo} --port 8080'';
+          };
+        };
+
+        networking.firewall.allowedTCPPorts = [ 8080 ];
       };
 
-      client2 = { ... }: {
+      client = { ... }: {
         virtualisation.diskSize = 40240;
         virtualisation.docker.enable = true;
-        virtualisation.docker.extraOptions = "--insecure-registry registry2:8080";
+        virtualisation.docker.extraOptions = "--insecure-registry registry:8080";
       };
+
     };
 
     testScript = ''
-          start_all()
+      start_all()
 
-          registry1.wait_for_open_port(8080)
-          registry1.wait_for_unit("wharfix.service")
-          client1.wait_for_unit("docker.service")
+      registry.wait_for_open_port(8080)
+      registry.wait_for_unit("wharfix.service")
+      client.wait_for_unit("docker.service")
 
-          registry2.wait_for_open_port(8080)
-          registry2.wait_for_unit("wharfix.service")
-          client2.wait_for_unit("docker.service")
-
-          client1.succeed("docker pull registry1:8080/kubernetes:master")
-          client2.fail("docker pull registry2:8080/kubernetes:master")
-        '';
+      client.${expectedResult}("docker pull registry:8080/kubernetes:master")
+    '';
 }
