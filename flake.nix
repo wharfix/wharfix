5c57b10a5420390363cde5d1d4fdb30029238686
Sarah Brofeldt <sarah@qtr.dk>
2025-12-04 07:09:48 +0100

    nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "wharfix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     crane = {
       url = "github:ipetkov/crane";
